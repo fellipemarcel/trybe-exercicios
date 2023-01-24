015d9b6e77a236c83954fc01d110c2af3fed2c0b
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-01-24 09:11:04 -0300

    Exercício para-fixar realizado

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/switch-e-case/para-fixar/scripts.js
@@ -0,0 +1,15 @@
+let statusDoCandidato = 'outro';
+
+switch (statusDoCandidato) {
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)!');
+        break;
+    case 'lista':
+        console.log('Você está na nossa lista de espera');
+        break;
+    case 'reprovada':
+        console.log('Você foi reprovada(o)');
+        break;
+    default:
+        console.log('Informação incorreta.');
+}

c929d062e4d73d2bd3c715f79d8829701ca2b0a9
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-01-25 09:04:27 -0300

    Exercício do INSS e IR finalizado

--- fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/agora-a-pratica/agora-a-pratica-11.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/agora-a-pratica/agora-a-pratica-11.js
@@ -1,8 +1,6 @@
 let aliquotaInss = 0;
 let aliquotaIr = 0;
 let salarioBruto = 3000;
-let salarioLiquido = 0;
-
 
 if (salarioBruto <= 1556.94) {
     aliquotaInss = (8 * salarioBruto) / 100;
@@ -16,17 +14,12 @@ if (salarioBruto <= 1556.94) {
 
 let salarioBase = salarioBruto - aliquotaInss;
 
-console.log(salarioBase);
-
 if (salarioBase <= 1903.98) {
     aliquotaIr = 0;
 } else if (salarioBase <= 2826.65) {
     aliquotaIr = ((7.5 * salarioBase) / 100) - 142.80;
 }
 
-console.log(aliquotaIr);
-// salario bruto = 3000,00
-// inss - 11% = 340,00
-// salário base = salario bruto - inss = 920,00
-// imposto de renda = até 
-// salario base = 2670,00
+let salarioLiquido = salarioBase - aliquotaIr;
+
+console.log(salarioLiquido);

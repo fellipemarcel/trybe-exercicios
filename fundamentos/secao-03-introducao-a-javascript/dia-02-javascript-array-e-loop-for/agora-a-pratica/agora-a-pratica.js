88a1698b3466c526330875cfec6b626ba753f6b0
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-01-25 11:22:23 -0300

    Requisito 2 'Some todos os valores...' realizado

--- fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/agora-a-pratica/agora-a-pratica.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-javascript-array-e-loop-for/agora-a-pratica/agora-a-pratica.js
@@ -1,5 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
 
 for (let index = 0; index < numbers.length; index += 1) {
-    console.log(numbers[index]);
+    // console.log(numbers[index]);
+    soma = soma + numbers[index];
 }

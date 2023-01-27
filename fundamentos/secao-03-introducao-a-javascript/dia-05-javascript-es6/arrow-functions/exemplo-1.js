18335b2507bc909d39cdb55429d90a358957cd16
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-01-27 16:47:20 -0300

    Realizando exercícios de 'arrow functions'

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-javascript-es6/arrow-functions/exemplo-1.js
@@ -0,0 +1,18 @@
+// const meuNome = function () {
+//     const primeiroNome = 'Fellipe';
+//     return primeiroNome;
+// }
+// console.log(meuNome());
+
+// const arrowFunction = () => {
+//     const phrase = 'Olá, mundo!';
+//     return phrase;
+// }
+
+// console.log(arrowFunction());
+
+// const printName = () => 'Fellipe';
+// console.log(printName());
+
+const multipliquePorDois = number => number * 2;
+console.log(multipliquePorDois(10));

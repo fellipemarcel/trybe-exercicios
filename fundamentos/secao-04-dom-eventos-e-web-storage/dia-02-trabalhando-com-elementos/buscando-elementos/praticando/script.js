ab6cf6080f32ddc978b3dae6a5edb45062270d1a
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-02-01 16:12:19 -0300

    Realizando Praticando

---
+++ fundamentos/secao-04-dom-eventos-e-web-storage/dia-02-trabalhando-com-elementos/buscando-elementos/praticando/script.js
@@ -0,0 +1,6 @@
+const elemento = console.log(document.getElementById('elementoOndeVoceEsta'));
+
+const paielemento = document.getElementById('elementoOndeVoceEsta').parentNode;
+paielemento.style.color = 'red';
+
+const primeiroFilho = console.log(document.getElementById('pai').firstChild);

d1847f7a5a73de243636f23eea2d1401e080e467
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-02-26 10:38:31 -0300

    Finalizei exercicio

---
+++ fundamentos/secao-07-intro-a-js-es6-e-testes-unitarios/exercicios-de-fixacao/fixacao-testes/createItem.test.js
@@ -0,0 +1,25 @@
+const createItem = require('./createItem');
+describe('a função createItem', () => {
+  it('cria um item válido', () => {
+    const expected = { name: 'banana', unit: 'kg', price: 1.99, quantity: 20 };
+    const actual = createItem('banana', 'kg', 1.99, 20);
+    expect(actual).toEqual(expected);
+  });
+  it('utiliza zero como quantidade padrão', () => {
+    const expected = 0;
+    const actual = createItem('banana', 'kg', 1.99);
+    expect(actual).toHaveProperty('quantity', expected);
+  });
+  it('Lança um erro quando não recebe parâmetros', () => {
+    expect(() => createItem()).toThrow();
+  });
+  it('Lança um erro se o nome do item não é uma string', () => {
+    expect(() => createItem(1.99, 'kg', 'banana', 20)).toThrow('O nome do item deve ser uma string');
+  });
+  it('Lança um erro se o preço é negativo', () => {
+    expect(() => createItem('banana', 'kg', -10, 1)).toThrow('O preço do item deve ser maior que zero');
+  });
+  it('Lança um erro se o preço é zero', () => {
+    expect(() => createItem('banana', 'kg', 0, 1)).toThrow('O preço do item deve ser maior que zero');
+  });
+});

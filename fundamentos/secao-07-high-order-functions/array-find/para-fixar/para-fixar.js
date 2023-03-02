619ec48221479734b497dd0081c1133c23a6a12b
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-03-02 15:08:51 -0300

    Lendo o conteúdo e entendendo exemplos

--- fundamentos/secao-07-high-order-functions/array-find/para-fixar/para-fixar.js
+++ fundamentos/secao-07-high-order-functions/array-find/para-fixar/para-fixar.js
@@ -1,3 +1,20 @@
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-// Adicione seu código aqui
+const isDivisible = numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+// console.log(isDivisible);
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const fiveLetters = names.find((element) => element.length === 5);
+
+// console.log(fiveLetters);
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ];
+  
+const match = musicas.find((music) => musicas.id = '31031685');
+console.log(match);

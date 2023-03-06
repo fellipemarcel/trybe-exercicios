af28509d7f677274582f7bf1ae8b902a328d505f
Fellipe Marcel Maia Silva <fmrclms@gmail.com>
2023-03-06 16:10:49 -0300

    Fazendo exemplos

---
+++ fundamentos/secao-07-high-order-functions/dia-03-higher-order-functions-filter-e-reduce/exemplo-1.js
@@ -0,0 +1,16 @@
+const users = [
+    {
+        username: 'fellipemarcel',
+        password: 'A35b12$50ABqT',
+        age: 27,
+    },
+    {
+        username: 'pedropascal',
+        password: 'KopD12794#!',
+        age: 40,
+    },
+];
+
+const filtering = () => 
+users.filter((user) => user.username === 'fellipemarcel');
+console.log(filtering());

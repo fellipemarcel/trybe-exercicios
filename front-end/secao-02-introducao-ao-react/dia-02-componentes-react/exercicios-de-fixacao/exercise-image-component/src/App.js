7130c78e6ad1fa78c6596d7e40701a6240cabfd5
Fellipe Marcel <fmrclms@gmail.com>
2023-03-29 09:18:02 -0300

    fiz exercicio de fixacao

---
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/exercicios-de-fixacao/exercise-image-component/src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+import Image from './Image';
+
+class App extends Component {
+  render() {
+    return (
+      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
+    )
+  }
+}
+
+export default App;

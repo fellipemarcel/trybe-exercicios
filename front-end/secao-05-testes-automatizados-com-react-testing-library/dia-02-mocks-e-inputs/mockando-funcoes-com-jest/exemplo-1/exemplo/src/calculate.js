e2e19f6664e12fdfab1c370aa9b8624285ad6dd8
Fellipe Marcel <fmrclms@gmail.com>
2023-04-29 17:04:44 -0300

    fiz exercicio de fixação

---
+++ front-end/secao-05-testes-automatizados-com-react-testing-library/dia-02-mocks-e-inputs/mockando-funcoes-com-jest/exemplo-1/exemplo/src/calculate.js
@@ -0,0 +1,3 @@
+const checkEvenNumber = (number) => number % 2 === 0 ? true : false;
+
+export default checkEvenNumber;
---
+++ front-end/secao-05-testes-automatizados-com-react-testing-library/dia-02-mocks-e-inputs/testando-apis/testing-apis/src/App.test.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+// afterEach(() => jest.clearAllMocks());
+
+// it('fetches a joke', async () => {
+//   const joke = {
+//     id: '7h3oGtrOfxc',
+//     joke: 'Whiteboards ... are remarkable.',
+//     status: 200,
+//   };
+
+//   jest.spyOn(global, 'fetch');
+//   global.fetch.mockResolvedValue({
+//     json: jest.fn().mockResolvedValue(joke),
+//   });
+
+//   // Outra forma de mock do fetch:
+
+//   /* global.fetch = jest.fn(() => Promise.resolve({
+//     json: () => Promise.resolve(joke),
+//   })); */
+
+//   // Mais uma forma de mock de fetch:
+
+//   /* global.fetch = jest.fn(async () => ({
+//   json: async () => joke
+// })); */
+
+//   render(<App />);
+//   const renderedJoke = await screen.findByText('Whiteboards ... are remarkable.');
+//   expect(renderedJoke).toBeInTheDocument();
+//   expect(global.fetch).toHaveBeenCalledTimes(1);
+//   expect(global.fetch).toHaveBeenCalledWith(
+//     'https://icanhazdadjoke.com/',
+//     { headers: { Accept: 'application/json' } },
+//   );
+// });
+
+it('ao renderizar a página, a primeira piada é exibida na tela', async () => {
+  const firstJoke = {
+    id: '7h3oGtrOfxc',
+    joke: 'Whiteboards ... are remarkable.',
+    status: 200,
+  };
+
+  const secondJoke = {
+    id: 'R7UfaahVfFd',
+    joke: 'My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.',
+    status: 200,
+  }
+
+  jest.spyOn(global, 'fetch');
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(firstJoke),
+  }); 
+
+  render(<App />);
+  expect(await screen.findByText(firstJoke.joke)).toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(1);
+  
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValue(secondJoke),
+  });
+  
+  const newJokeBtn = screen.getByRole('button', { name: /new joke/i })
+  userEvent.click(newJokeBtn);
+  expect(await screen.findByText(secondJoke.joke)).toBeInTheDocument();
+  expect(screen.queryByText(firstJoke)).not.toBeInTheDocument();
+  expect(global.fetch).toBeCalledTimes(2);
+});
